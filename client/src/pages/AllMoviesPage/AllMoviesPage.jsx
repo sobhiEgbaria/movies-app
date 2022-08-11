407ff5e4d0d19d535f1240b830b1c58c3237f238
sobhiEgbaria <sobhim686@gmail.com>
2022-08-11 18:13:41 +0300

    update all movies/tv pages

--- client/src/pages/AllMoviesPage/AllMoviesPage.jsx
+++ client/src/pages/AllMoviesPage/AllMoviesPage.jsx
@@ -7,14 +7,30 @@ import { Row, Col } from "react-bootstrap";
 import MoviesCard from "../../components/MoviesCard";
 import "./AllMoviesPage.css";
 
+// in this component i use promise all to fetch
 const AllMoviesPage = ({ page, setPage }) => {
   const [allMoviesData, setAllMoviesData] = useState([]);
   useEffect(() => {
-    const fetchAllMovies = async () => {
-      const { data } = await axios.get(
+    const fetchAllMovies = () => {
+      const data1 = fetch(
         `https://api.themoviedb.org/3/discover/movie?api_key=8c5382be42ac80e40fd763bc48f73c07&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate`
       );
-      setAllMoviesData(data.results);
+      const data2 = fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=8c5382be42ac80e40fd763bc48f73c07&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${
+          page + 1
+        }&with_watch_monetization_types=flatrate`
+      );
+
+      Promise.all([data1, data2])
+        .then((allData) => {
+          return Promise.all(allData.map((item) => item.json()));
+        })
+
+        .then((allData) => {
+          const all = [...allData[0].results, ...allData[1].results];
+
+          setAllMoviesData(all);
+        });
     };
     fetchAllMovies();
   }, [allMoviesData]);
@@ -27,7 +43,7 @@ const AllMoviesPage = ({ page, setPage }) => {
       <main>
         <Row>
           {allMoviesData.map((item, index) => (
-            <Col key={index} sm={11} md={5} lg={3} xl={2}>
+            <Col key={index} sm={11} md={6} lg={4} xl={3}>
               <MoviesCard
                 key={index}
                 title={item.title}
